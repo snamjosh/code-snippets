ba988e21061eaba10b9b852428d207fe122f73a4
snamjosh <38986112+snamjosh@users.noreply.github.com>
2026-04-15 09:02:46 -0700

    Create DTWiget_Html.js

---
+++ Service Portal Widgets/Dark Theme Widget/DTWiget_Html.js
@@ -0,0 +1,7 @@
+<div class="dark-mode-toggle-container">
+  <button class="btn dark-mode-toggle" 
+          ng-click="c.toggleDarkMode()"
+          aria-label="Toggle Dark Mode">
+    <i class="fa" ng-class="c.isDarkMode ? 'fa-sun-o' : 'fa-moon-o'"></i>
+  </button>
+</div>
